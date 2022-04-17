11d5b39336762b159975313799df73d9e1a389ec
Nicole Mills <nam0068@auburn.edu>
2022-04-17 01:47:44 -0500

    create production class

---
+++ InhertianceApp/ProductionWorker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InhertianceApp
+{
+    class ProductionWorker :Employee
+    {
+        public int Shift {get; set;}
+        public double HourlyPayRate {get; set;}
+    }
+}
