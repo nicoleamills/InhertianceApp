b89a7fe5d317994b02ff4adf65db9eeec9e8b4ec
Nicole Mills <nam0068@auburn.edu>
2022-04-17 01:37:42 -0500

    create form

--- InhertianceApp/Form1.Designer.cs
+++ InhertianceApp/Form1.Designer.cs
@@ -28,112 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtEmpName = new System.Windows.Forms.TextBox();
+            this.lblEmpName = new System.Windows.Forms.Label();
+            this.lblEmpNum = new System.Windows.Forms.Label();
+            this.lblShift = new System.Windows.Forms.Label();
+            this.lblPayRate = new System.Windows.Forms.Label();
+            this.txtEmpNum = new System.Windows.Forms.TextBox();
+            this.txtShift = new System.Windows.Forms.TextBox();
+            this.txtPayRate = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtEmpName
             // 
-            this.textBox1.Location = new System.Drawing.Point(348, 134);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtEmpName.Location = new System.Drawing.Point(348, 134);
+            this.txtEmpName.Name = "txtEmpName";
+            this.txtEmpName.Size = new System.Drawing.Size(100, 20);
+            this.txtEmpName.TabIndex = 0;
+            this.txtEmpName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // label1
+            // lblEmpName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(205, 134);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.lblEmpName.AutoSize = true;
+            this.lblEmpName.Location = new System.Drawing.Point(205, 134);
+            this.lblEmpName.Name = "lblEmpName";
+            this.lblEmpName.Size = new System.Drawing.Size(84, 13);
+            this.lblEmpName.TabIndex = 1;
+            this.lblEmpName.Text = "Employee Name";
+            this.lblEmpName.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // lblEmpNum
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(208, 174);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.lblEmpNum.AutoSize = true;
+            this.lblEmpNum.Location = new System.Drawing.Point(208, 174);
+            this.lblEmpNum.Name = "lblEmpNum";
+            this.lblEmpNum.Size = new System.Drawing.Size(96, 13);
+            this.lblEmpNum.TabIndex = 2;
+            this.lblEmpNum.Text = "Employee Number:";
+            this.lblEmpNum.Click += new System.EventHandler(this.label2_Click);
             // 
-            // label3
+            // lblShift
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(208, 214);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "label3";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.lblShift.AutoSize = true;
+            this.lblShift.Location = new System.Drawing.Point(208, 214);
+            this.lblShift.Name = "lblShift";
+            this.lblShift.Size = new System.Drawing.Size(31, 13);
+            this.lblShift.TabIndex = 3;
+            this.lblShift.Text = "Shift:";
+            this.lblShift.Click += new System.EventHandler(this.label3_Click);
             // 
-            // label4
+            // lblPayRate
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(208, 255);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "label4";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
+            this.lblPayRate.AutoSize = true;
+            this.lblPayRate.Location = new System.Drawing.Point(208, 255);
+            this.lblPayRate.Name = "lblPayRate";
+            this.lblPayRate.Size = new System.Drawing.Size(54, 13);
+            this.lblPayRate.TabIndex = 4;
+            this.lblPayRate.Text = "Pay Rate:";
+            this.lblPayRate.Click += new System.EventHandler(this.label4_Click);
             // 
-            // textBox2
+            // txtEmpNum
             // 
-            this.textBox2.Location = new System.Drawing.Point(348, 174);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 5;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.txtEmpNum.Location = new System.Drawing.Point(348, 174);
+            this.txtEmpNum.Name = "txtEmpNum";
+            this.txtEmpNum.Size = new System.Drawing.Size(100, 20);
+            this.txtEmpNum.TabIndex = 5;
+            this.txtEmpNum.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // textBox3
+            // txtShift
             // 
-            this.textBox3.Location = new System.Drawing.Point(348, 214);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 6;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.txtShift.Location = new System.Drawing.Point(348, 214);
+            this.txtShift.Name = "txtShift";
+            this.txtShift.Size = new System.Drawing.Size(100, 20);
+            this.txtShift.TabIndex = 6;
+            this.txtShift.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
-            // textBox4
+            // txtPayRate
             // 
-            this.textBox4.Location = new System.Drawing.Point(348, 255);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 7;
-            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.txtPayRate.Location = new System.Drawing.Point(348, 255);
+            this.txtPayRate.Name = "txtPayRate";
+            this.txtPayRate.Size = new System.Drawing.Size(100, 20);
+            this.txtPayRate.TabIndex = 7;
+            this.txtPayRate.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
-            // button1
+            // btnSubmit
             // 
-            this.button1.Location = new System.Drawing.Point(348, 300);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnSubmit.Location = new System.Drawing.Point(348, 300);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(100, 23);
+            this.btnSubmit.TabIndex = 8;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtPayRate);
+            this.Controls.Add(this.txtShift);
+            this.Controls.Add(this.txtEmpNum);
+            this.Controls.Add(this.lblPayRate);
+            this.Controls.Add(this.lblShift);
+            this.Controls.Add(this.lblEmpNum);
+            this.Controls.Add(this.lblEmpName);
+            this.Controls.Add(this.txtEmpName);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -144,15 +145,15 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtEmpName;
+        private System.Windows.Forms.Label lblEmpName;
+        private System.Windows.Forms.Label lblEmpNum;
+        private System.Windows.Forms.Label lblShift;
+        private System.Windows.Forms.Label lblPayRate;
+        private System.Windows.Forms.TextBox txtEmpNum;
+        private System.Windows.Forms.TextBox txtShift;
+        private System.Windows.Forms.TextBox txtPayRate;
+        private System.Windows.Forms.Button btnSubmit;
     }
 }
 
--- InhertianceApp/Form1.cs
+++ InhertianceApp/Form1.cs
@@ -61,5 +61,10 @@ namespace InhertianceApp
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
